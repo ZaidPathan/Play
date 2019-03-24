46e451b10945b5b8673d871fbfb111045e8fe75d
Zaid Pathan <zaidkhanintel@gmail.com>
2019-03-24 21:45:50 +0530

    Initial viewcontroller setup.

---
+++ Play/PlayModule/PlayModule.h
@@ -0,0 +1,19 @@
+//
+//  PlayModule.h
+//  PlayModule
+//
+//  Created by Zaid Pathan on 24/03/19.
+//  Copyright © 2019 Zaid Pathan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PlayModule.
+FOUNDATION_EXPORT double PlayModuleVersionNumber;
+
+//! Project version string for PlayModule.
+FOUNDATION_EXPORT const unsigned char PlayModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PlayModule/PublicHeader.h>
+
+
